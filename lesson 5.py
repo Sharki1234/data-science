2a100f3d6b6ce1bf2454eb7243359b48ae24c89c
Shkarki1234 <thanishkavenkata28@gmail.com>
2026-06-23 19:09:36 +0100

    Add files via upload

---
+++ lesson 5.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#getting dataset
+dataset = pd.read_csv(r"C:\Users\rakhi\Downloads\Data (1).csv")
+x = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,-1].values
+
+#filling in missing values
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan,strategy="mean")
+imputer.fit(x[:,1:3])
+x[:,1:3] = imputer.transform(x[:,1:3])
+
+#encoding independant variables
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[("encoder",OneHotEncoder(),[0])],remainder= "passthrough")
+x = np.array(ct.fit_transform(x))
+print(x)#makes France and Spain and GErmany into binary patters like 0,0,1 or 1,0,0
+
+#encoding dependant variables
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+y = le.fit_transform(y)
+print(y)
+
---
+++ practice_lesson_6.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import matplotlib as plt
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import metrics
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+
+iris = pd.read_csv(r"C:\Users\rakhi\Downloads\iris.csv")
+
+
+y = iris["species"]
+x = iris.drop("species",axis=1)#data w/out species
+le = LabelEncoder()
+y = le.fit_transform(iris["species"])
+
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=1)
+
+model = DecisionTreeClassifier(max_depth=3,random_state=1)
+
+model.fit(x_train,y_train)
+
+predictions = model.predict(x_test)
+
+print("Accuracy",metrics.accuracy_score(predictions,y_test))
