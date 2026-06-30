2fd5751270f30ffcf88d74771f4d75fe9ea32e1c
Sharki1234 <thanishkavenkata28@gmail.com>
2026-06-30 19:54:14 +0100

    timethingy

---
+++ lesson_8.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import plotly
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+data = pd.read_csv(r"C:\Users\rakhi\Downloads\WHO-COVID-19-global-data (1).csv")
+
+data["DateReported"] = pd.to_datetime(data["DateReported"])
+
+data_dates = data.groupby("DateReported").sum()
+
+figure = go.Figure()
+figure.add_trace(go.Scatter(x = data_dates.index,y = data_dates["Cumulative_cases"],line_color="blue"))
+#figure.show()
+
+figure2 = go.Figure()
+figure2.add_trace(go.Scatter(x = data_dates.index,y = data_dates["New_cases"]))
+#figure2.show()
+
+data_France = data[(data["Country"]=="France")]
+figure3 = go.Figure()
+figure3.add_trace(go.Scatter(x = data_France["DateReported"],y = data_France["New_cases"]))
+figure3.show()
